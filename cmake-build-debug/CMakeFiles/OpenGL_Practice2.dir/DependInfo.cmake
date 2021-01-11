0b2931a1a4f0682d5f3409e5e5095bce2c83a2f0
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2021-01-10 20:09:55 -0500

    Added GLEW library

--- cmake-build-debug/CMakeFiles/OpenGL_Practice2.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/OpenGL_Practice2.dir/DependInfo.cmake
@@ -8,9 +8,15 @@ set(CMAKE_DEPENDS_CHECK_CXX
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_NO_GLU=1"
+  )
+
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../dependencies/glfw-3.3.2/include"
+  "../dependencies/glew-2.2.0/include"
   )
 
 # Targets to which this target links.
